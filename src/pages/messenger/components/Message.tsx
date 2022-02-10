41103f0b912ec76b115474e919b28fe00e174ac9
gilpop8663 <wolfye@naver.com>
2022-02-11 06:40:35 +0900

    Add -데이터를 받아서 이용할  메세지 컴포넌트 파일 , 스타일 파일 추가

--- src/pages/messenger/components/Message.tsx
+++ src/pages/messenger/components/Message.tsx
@@ -1,5 +1,53 @@
-import React from 'react';
+import MessageStyle from 'assets/styles/MessageStyle';
 
-export default function Message() {
-  return <div>Message</div>;
+const {
+  Container,
+  ProfileImage,
+  MessageWrapper,
+  UserNameDateFunction,
+  UserNameDate,
+  MessageFunction,
+  AnswerBtn,
+  DeleteBtn,
+  UserName,
+  MessageDate,
+  MessageText,
+  AreYouAuthor,
+} = MessageStyle;
+
+interface messagesProps {
+  userId: string;
+  userName: string;
+  profileImage: string;
+  content: string;
+  date: string;
+}
+
+interface messageComponentProps {
+  attr: messagesProps;
+}
+
+export default function Message({ attr }: messageComponentProps) {
+  const { userId, userName, profileImage, content, date } = attr;
+  return (
+    <Container>
+      <ProfileImage src={profileImage} />
+      <MessageWrapper>
+        <UserNameDateFunction>
+          <UserNameDate>
+            <UserName>
+              {userName}
+              {userId && <AreYouAuthor>*</AreYouAuthor>}
+            </UserName>
+            <MessageDate>{date}</MessageDate>
+          </UserNameDate>
+          <MessageFunction>
+            {userId && <DeleteBtn>삭제하기</DeleteBtn>}
+            {!userId && <AnswerBtn>답장하기</AnswerBtn>}
+          </MessageFunction>
+        </UserNameDateFunction>
+        <MessageText>{content}</MessageText>
+      </MessageWrapper>
+    </Container>
+  );
 }
