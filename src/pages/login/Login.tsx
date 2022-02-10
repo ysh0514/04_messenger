581ddafd016be6c1e78672d188f21de62a465fdf
ydh94 <dbeogud9411@gmail.com>
2022-02-11 04:26:19 +0900

    Add - login layout

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,64 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import LOGO from '../../assets/images/logo.svg';
+import LoginStyle from 'assets/styles/LoginStyle';
+
+const {
+  Container,
+  LogoBox,
+  InputContainer,
+  Input,
+  BtnContainer,
+  ErrorBox,
+  LoginBtn,
+  SignupBtn,
+} = LoginStyle;
 
 export default function Login() {
-  return <div>Login</div>;
+  const [loginInfo, setLoginInfo] = useState({ id: '', password: '' });
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleInputValue = (key) => (e) => {
+    setLoginInfo({ ...loginInfo, [key]: e.target.value.toLowerCase() });
+    setErrorMsg('');
+  };
+
+  const handleLogin = () => {
+    console.log('login');
+    setErrorMsg('아이디 또는 비밀번호를 확인해주세요');
+  };
+
+  const handleKeyPress = (e: Event) => {
+    if (e.type === 'keypress' && e.code === 'Enter') {
+      handleLogin();
+    }
+  };
+
+  useEffect(() => {
+    console.log(loginInfo);
+  }, [loginInfo]);
+
+  return (
+    <Container>
+      <LogoBox src={LOGO} />
+      <InputContainer>
+        <Input
+          placeholder="ID"
+          type="id"
+          onChange={handleInputValue('id')}
+          onKeyPress={handleKeyPress}
+        />
+        <Input
+          placeholder="PW"
+          type="password"
+          onChange={handleInputValue('password')}
+          onKeyPress={handleKeyPress}
+        />
+        <ErrorBox>{errorMsg}</ErrorBox>
+      </InputContainer>
+      <BtnContainer>
+        <LoginBtn onClick={handleLogin}>로그인</LoginBtn>
+        <SignupBtn>회원가입</SignupBtn>
+      </BtnContainer>
+    </Container>
+  );
 }
