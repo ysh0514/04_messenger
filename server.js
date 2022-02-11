1b754b8bd8ce73205467a44cc81c0c13421ba39a
ydh94 <dbeogud9411@gmail.com>
2022-02-11 20:24:56 +0900

    fix

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const router = jsonServer.router('./db.json');
 const port = process.env.PORT || 4000;
 
 server.use(router);
+server.get('/', (req, res) => {
+  res.send({ hi: 'hi', a: req.query });
+});
 
 server.listen(port, () => {
   console.log('server is running');
