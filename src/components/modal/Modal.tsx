2d39fb3b2049ef8f477979c9a62d0724e1af0fc5
limbs722 <limbs722@gmail.com>
2022-02-11 17:33:01 +0900

    Feat - Modal 기능 구현
    모달 기능 추가

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,5 +1,45 @@
-import React from 'react';
+import ModalStyle from '../../assets/styles/ModalStyle';
 
-export default function Modal() {
-  return <div>Modal</div>;
+const { ModalWrapper } = ModalStyle;
+
+interface ModalProps {
+  isShow: boolean;
+  type: string;
+  header: string;
+  content: object; // 삭제 대상
+  onClick: (type: string) => void;
+}
+
+export default function Modal(attr: ModalProps) {
+  const { isShow, header, type, content, onClick } = attr;
+  function getButtons() {
+    // 일단 대화 삭제 경우만 작성하였음
+    const btnGroup = [
+      <button key="cancel" onClick={() => onClick('cancel')}>
+        취소
+      </button>,
+      <button key="delete" onClick={() => onClick('delete')}>
+        삭제
+      </button>,
+    ];
+    return btnGroup;
+  }
+
+  console.log(isShow);
+
+  return (
+    <ModalWrapper isShow={isShow}>
+      <section>
+        <header>
+          {header}
+          <button className="close" onClick={() => onClick('cancel')}>
+            {' '}
+          </button>
+        </header>
+        {/* <main>{content}</main> */}
+        <main>삭제할 대화</main>
+        <footer>{getButtons()}</footer>
+      </section>
+    </ModalWrapper>
+  );
 }
