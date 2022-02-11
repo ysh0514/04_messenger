4cedcd1cc02072aa1602fd34718d5c1157eec708
ydh94 <dbeogud9411@gmail.com>
2022-02-11 20:15:50 +0900

    heroku

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults({
-  static: './build',
-});
+
 const port = process.env.PORT || 4000;
 
-server.use(middlewares);
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
