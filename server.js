bba18d445364e08c48d938245a09383b6108f7bf
ydh94 <dbeogud9411@gmail.com>
2022-02-11 21:07:48 +0900

    heroku

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json');
-const port = process.env.PORT || 4000;
-
-server.use(router);
-server.get('/', (req, res) => {
-  res.send({ hi: 'hi', a: req.query });
+const middlewares = jsonServer.defaults({
+  static: './build',
 });
 
-server.listen(port, () => {
+server.use(middlewares);
+
+const { users } = require('./db.json');
+
+server.get('/users', async (req, res) => {
+  const isQuery = !!Object.keys(req.query).length;
+  if (isQuery) {
+    const userInfo = await users.find((e) => {
+      return e.id === req.query.id && e.password === req.query.password;
+    });
+    res.send({ ...userInfo });
+  } else {
+    res.send(users);
+  }
+});
+
+const { messages } = require('./db.json');
+
+server.get('/messages', (req, res) => {
+  res.send(messages);
+});
+
+server.post('/messages', (req, res) => {
+  const { userId, userName, profileImage, content, date } = req.body;
+  messages.push({ userId, userName, profileImage, content, date });
+  res.send(messages);
+});
+
+server.listen(4000, () => {
   console.log('server is running');
 });
