723a1c61f35a5fe1fc9665aab8dd21ef34a41875
gilpop8663 <wolfye@naver.com>
2022-02-11 16:20:26 +0900

    Add - 채팅이 올라올 때 스크롤이 아래로 향할 컴포넌트를 추가 . 그리고 메세지 css 추가

--- src/assets/styles/MessageStyle.ts
+++ src/assets/styles/MessageStyle.ts
@@ -2,12 +2,17 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
+  align-items: center;
+  margin: 20px 0px;
+  padding: 0px 20px;
 `;
 
 const ProfileImage = styled.img`
   width: 40px;
   height: 40px;
+  margin-right: 20px;
   border-radius: 20px;
+  cursor: pointer;
 `;
 
 const MessageWrapper = styled.div`
@@ -17,25 +22,46 @@ const MessageWrapper = styled.div`
 
 const UserNameDateFunction = styled.div`
   display: flex;
+  margin-bottom: 10px;
+  justify-content: space-between;
+  align-items: center;
+  width: 93vw;
 `;
 
 const UserNameDate = styled.div``;
 
 const MessageFunction = styled.div``;
 
-const AnswerBtn = styled.button``;
-
-const DeleteBtn = styled.button``;
-
-const UserName = styled.span``;
-
-const MessageDate = styled.span``;
-
-const MessageText = styled.span`
-  white-space: pre-wrap;
+const AnswerBtn = styled.button`
+  cursor: pointer;
+  padding: 4px 8px;
+  border-radius: 20px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  font-size: 12px;
+  margin-left: 10px;
 `;
 
-const AreYouAuthor = styled.span``;
+const DeleteBtn = styled(AnswerBtn)``;
+
+const UserName = styled.span`
+  font-size: 16px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const MessageDate = styled.span`
+  font-size: 12px;
+  margin-left: 10px;
+`;
+
+const MessageText = styled.span`
+  white-space: pre-line;
+  font-size: 14px;
+`;
+
+const AreYouAuthor = styled.span`
+  margin-left: 3px;
+`;
 
 const MessageStyle = {
   Container,
