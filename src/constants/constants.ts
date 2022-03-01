1bc105320ff560e58275e3e11ab3bf6c7c21a24b
gilpop8663 <wolfye@naver.com>
2022-03-01 14:00:20 +0900

    Fix - BASEURL , package.json의 script 변경

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,7 +6,7 @@ export const LOCATION = 'Asia/Seoul';
 export const DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 
 //SERVER
-export const BASE_URL = 'https://json-server-wanted14.herokuapp.com';
+export const BASE_URL = 'https://dry-hollows-03672.herokuapp.com';
 export const MESSEAGE_URL = `${BASE_URL}/messages`;
 export const LOGIN_URL = `${BASE_URL}/login`;
 export const PARAMS_USERS_URL = '/users';
